e4d1ebf94a94c17f30702d4956a273fa2daefcb3
Bohdan Khudiakov <b.khudiakov@gmail.com>
2025-02-11 16:37:34 +0200

    Added / to the end of some endpoints

--- cinema/urls.py
+++ cinema/urls.py
@@ -11,8 +11,6 @@ from cinema.views import (
 )
 
 
-app_name = "cinema"
-
 cinema_hall_list = CinemaHallViewSet.as_view(
     actions={"get": "list", "post": "create"}
 )
@@ -31,12 +29,14 @@ urlpatterns = [
     path("", include(router.urls)),
     path("genres/", GenreList.as_view(), name="genre-list"),
     path("actors/", ActorList.as_view(), name="actor-list"),
-    path("genres/<int:pk>", GenreDetail.as_view(), name="genre-detail"),
-    path("actors/<int:pk>", ActorDetail.as_view(), name="actor-detail"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
     path("cinema_halls/", cinema_hall_list, name="cinema-hall-list"),
     path(
-        "cinema_halls/<int:pk>",
+        "cinema_halls/<int:pk>/",
         cinema_hall_detail,
         name="cinema-hall-detail"
     )
 ]
+
+app_name = "cinema"
